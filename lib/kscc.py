2f37d123568509af5d9ea2887687c562325abfd0
Federico Úsuga Agudelo <1039459852@udea.local>
2024-05-06 16:15:11 -0500

    parallelized version

--- lib/kscc.py
+++ lib/kscc.py
@@ -1,26 +1,7 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-'''Noted by Tai Dinh
-This file is used to run the Silhouette based algorithm for categorical data clustering (k-SCC algorithm),
-using the Kernel density estimation approach and information theoretic based dissimilarity measure
-Use simple matching measure for calculate the silhouette distance matrix.
-'''
-import shutil
-import sys, os
-from collections import defaultdict
-from lib import kscc_unsupervised as us
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 import numpy as np
 import math
-
-
-def makedirs(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
-    else:
-        shutil.rmtree(path)  # Removes all the subdirectories!
-        os.makedirs(path)
+from collections import defaultdict
+from joblib import Parallel, delayed
 
 '''
 This function is used to calculate the dissimilarity between 2 attributes x and y at the iattr(d)
@@ -40,8 +21,26 @@ def attr_dissim(x, y, iattr, global_attr_freq):
         numerator = 2 * math.log(global_attr_freq[iattr][x])
     else:
         numerator = 2 * math.log((global_attr_freq[iattr][x] + global_attr_freq[iattr][y]))
-    denominator = math.log(global_attr_freq[iattr][x]) + math.log(global_attr_freq[iattr][y])
+    denominator = math.log(global_attr_freq[iattr][x]) + math.log(global_attr_freq[iattr][y]) #Noted by Tai Dinh, Equation 21, page 124
     return 1 - numerator / denominator
+
+def pair_distance(a,b,n_attrs, global_attr_freq):
+    distance = 0.
+    for j in range(n_attrs):
+            distance += attr_dissim(a[j], b[j], j, global_attr_freq)
+    return distance
+
+def pairwise_distances(x):
+    n_samples, n_attrs = x.shape
+    distance = np.zeros((n_samples,n_samples))
+    global_attr_freq = _cal_global_attr_freq(x, n_samples, n_attrs)
+    for row in range(n_samples):
+        col = row + 1
+        while col < n_samples:
+            distance[row,col] = pair_distance(x[row],x[col], n_attrs, global_attr_freq)
+            distance[col,row] = distance[row,col]
+            col+=1
+    return distance
 
 '''
 This function is used to calculate the dissimilarity between a centroid and a vector a
@@ -56,13 +55,11 @@ def vector_matching_dissim(centroid, a, global_attr_freq):
             distance += curc[key] * attr_dissim(key, a[ic], ic, global_attr_freq)
     return distance
 
-
 '''
 This function is used to calculate the distances between centroid clusters and a data point, using the global_attr_freq.
 global_axttr_freq[i][x] is the probability of the attribute at position i and value x on the whole samples.
 categorical is the set of categorical attributes in the data point.
 '''
-
 
 def vectors_matching_dissim(vectors, a, global_attr_freq):
     '''Get nearest vector in vectors to a'''
@@ -101,7 +98,7 @@ def move_point_between_clusters(point, ipoint, to_clust, from_clust,
 
 def matching_dissim(a, b):
     '''Simple matching dissimilarity function'''
-    return np.sum(a != b)
+    return np.sum(a != b, axis=1)
 
 '''
 This function is used to calculate the lambda as the formula in slide page 15
@@ -230,7 +227,7 @@ def kscc_kernel_it_iter(X, centroids, cl_attr_freq, membership, global_attr_freq
 '''
 
 
-def _labels(X, centroids, global_attr_freq):
+def _labels_cost(X, centroids, global_attr_freq):
     '''
     Calculate labels and cost function given a matrix of points and
     a list of centroids for the k-modes algorithm.
@@ -280,67 +277,79 @@ def _init_clusters(X, centroids, n_clusters, nattrs, npoints, verbose):
 
     return cl_attr_freq, membership
 
-def kscc_kernel_it(X, n_clusters, n_init, verbose):
+def kscc_kernel_it_single(X, n_clusters, init_no, verbose):
     X = np.asanyarray(X)
     npoints, nattrs = X.shape
     assert n_clusters < npoints, "More clusters than data points?"
 
-    all_centroids = []
-    all_labels = []
-    all_costs = []
-
     global global_attr_freq
     global_attr_freq = _cal_global_attr_freq(X, npoints, nattrs)
 
-    for init_no in range(n_init):
-        seeds = np.random.choice(range(npoints), n_clusters)
-        centroids = X[seeds]
-        cl_attr_freq, membership = _init_clusters(X, centroids, n_clusters, nattrs, npoints, verbose)
+    seeds = np.random.choice(range(npoints), n_clusters)
+    centroids = X[seeds]
+    cl_attr_freq, membership = _init_clusters(X, centroids, n_clusters, nattrs, npoints, verbose)
 
-        centroids = [[defaultdict(float) for _ in range(nattrs)]
-                     for _ in range(n_clusters)]
+    centroids = [[defaultdict(float) for _ in range(nattrs)]
+                 for _ in range(n_clusters)]
 
-        # Perform initial centroid update
-        lbd = np.zeros(n_clusters, dtype='float')
-        for ik in range(n_clusters):
-            cluster_members = sum(membership[ik, :])
-            for iattr in range(nattrs):
-                centroids[ik][iattr] = cal_centroid_value(lbd[ik], cl_attr_freq[ik][iattr], cluster_members,
-                                                      len(cl_attr_freq[ik][iattr].keys()))
-        # __ITERATION__
+    # Perform initial centroid update
+    lbd = np.zeros(n_clusters, dtype='float')
+    for ik in range(n_clusters):
+        cluster_members = sum(membership[ik, :])
+        for iattr in range(nattrs):
+            centroids[ik][iattr] = cal_centroid_value(lbd[ik], cl_attr_freq[ik][iattr], cluster_members,
+                                                  len(cl_attr_freq[ik][iattr].keys()))
+    # __ITERATION__
+    if verbose:
+        print("Starting iterations...")
+    labels = None
+    converged = False
+    cost = np.Inf
+    while not converged:
         if verbose:
-            print("Starting iterations...")
-        labels = None
-        converged = False
-        cost = np.Inf
-        while not converged:
-            if verbose:
-                print("...kn_it_local loop")
-            centroids, moves, lbd = kscc_kernel_it_iter(X, centroids, cl_attr_freq, membership, global_attr_freq, lbd)
-            if verbose:
-                print("...Update labels, costs")
-            labels, ncost = _labels(X, centroids, global_attr_freq)
-            converged = (moves == 0) or (ncost >= cost)
-            cost = ncost
-            if verbose:
-                print("Run {}, moves: {}, cost: {}"
-                      .format(init_no + 1, moves, cost))
-        # Store result of current runt
-        all_centroids.append(centroids)
-        all_labels.append(labels)
-        all_costs.append(cost)
+            print("...kn_it_local loop")
+        centroids, moves, lbd = kscc_kernel_it_iter(X, centroids, cl_attr_freq, membership, global_attr_freq, lbd)
+        if verbose:
+            print("...Update labels, costs")
+        labels, ncost = _labels_cost(X, centroids, global_attr_freq)
+        converged = (moves == 0) or (ncost >= cost)
+        cost = ncost
+        if verbose:
+            print("Run {}, moves: {}, cost: {}"
+                  .format(init_no + 1, moves, cost))
+
+    return centroids, labels, cost
+
+def kscc_kernel_it(X, n_clusters, n_init, verbose, n_jobs):
+    """step 1 k-SCC algortihm"""
+    n_points, n_attrs = X.shape
+    assert n_clusters <= n_points, f"Cannot have more clusters ({n_clusters}) " \
+                                   f"than data points ({n_points})."
+
+    results = []
+    if n_jobs == 1:
+        for init_no in range(n_init):
+            results.append(kscc_kernel_it_single(
+                X, n_clusters, init_no, verbose 
+            ))
+    else:
+        results = Parallel(n_jobs=n_jobs, verbose=0)(
+            delayed(kscc_kernel_it_single)(X, n_clusters, init_no, verbose)
+            for init_no in range(n_init))
+    all_centroids, all_labels, all_costs = zip(*results)
 
     best = np.argmin(all_costs)
     if n_init > 1 and verbose:
-        print("Best run was number {}, min cost is {}".format(best + 1, all_costs[best]))
+        print(f"Best run was number {best + 1}")
 
     return all_centroids[best], all_labels[best], all_costs[best]
 
 class KSCC_Kernel_IT(object):
-    def __init__(self, n_clusters,n_init, verbose=1):
+    def __init__(self, n_clusters, n_init=10, verbose=1, n_jobs=1):
         self.n_clusters = n_clusters
         self.n_init = n_init
         self.verbose = verbose
+        self.n_jobs = n_jobs
 
     def fit(self, X, **kwargs):
         '''Compute k-representative clustering.
@@ -350,7 +359,7 @@ class KSCC_Kernel_IT(object):
         X : array-like, shape=[n_samples, n_features]
         '''
 
-        self.cluster_centroids_, self.labels_, self.cost_ = kscc_kernel_it(X, self.n_clusters, self.n_init, self.verbose)
+        self.cluster_centroids_, self.labels_, self.cost_ = kscc_kernel_it(X, self.n_clusters, self.n_init, self.verbose, self.n_jobs)
         return self
 
     def fit_predict(self, X, **kwargs):
@@ -374,107 +383,6 @@ class KSCC_Kernel_IT(object):
         labels : array, shape [n_samples,]
             Index of the cluster each sample belongs to.
         '''
-        # assert hasattr(self, 'cluster_centroids_'), "Model not yet fitted"
-        # return _labels_cost(X, self.cluster_centroids_)[0]
-
-def pairwise_distances(x):
-    n_samples, n_attrs = x.shape
-    distance = np.zeros((n_samples,n_samples))
-    for row in range(n_samples):
-        col = row + 1
-        while col < n_samples:
-            distance[row,col] = matching_dissim(x[row],x[col])
-            distance[col,row] = distance[row,col]
-            col+=1
-    return distance
-
-
-def run(argv):
-    n_init = 100
-    ifile = "sake.csv"
-    ofile = "kscc_sm_sake.csv"
-    output_folder = "output"
-    makedirs(output_folder)
-    delim = ","
-    verbose = 0
-
-    # Get samples & labels
-    # if not use_first_column_as_label:
-    x = np.genfromtxt(ifile, dtype = str, delimiter = delim)[:]
-    result = []
-    # range(2, x.shape[0] - 1)
-    for n_clusters in range(2, 11):
-        # Create a subplot with 1 row and 2 columns
-        fig1, ax1 = plt.subplots()
-        # fig.set_size_inches(18, 7)
-
-        # The 1st subplot is the silhouette plot
-        # The silhouette coefficient can range from -1, 1 but in this example all
-        # lie within [-0.1, 1]
-        ax1.set_xlim([-0.1, 1])
-        # The (n_clusters+1)*10 is for inserting blank space between silhouette
-        # plots of individual clusters, to demarcate them clearly.
-        ax1.set_ylim([0, len(x) + (n_clusters + 1) * 10])
-
-        kr = KSCC_Kernel_IT(n_clusters=n_clusters, n_init=n_init, verbose= verbose)
-        kr.fit_predict(x)
-        cluster_labels = kr.labels_
-
-
-        # The silhouette_score gives the average value for all the samples.
-        # This gives a perspective into the density and separation of the formed
-        # clusters
-        _distance = pairwise_distances(x)
-        sample_silhouette_values, silhouette_avg = us.silhouette_score(x, cluster_labels,_distance)
-        print("For n_clusters =", n_clusters,
-              "The average silhouette_score is :", silhouette_avg)
-        result.append(silhouette_avg)
-
-        y_lower = 10
-        for i in range(n_clusters):
-            # Aggregate the silhouette scores for samples belonging to
-            # cluster i, and sort them
-            ith_cluster_silhouette_values = \
-                sample_silhouette_values[cluster_labels == i]
-
-            ith_cluster_silhouette_values.sort()
-
-            size_cluster_i = ith_cluster_silhouette_values.shape[0]
-            y_upper = y_lower + size_cluster_i
-
-            # cmap = cm.get_cmap("Spectral")
-            # color = cmap(float(i) / n_clusters)
-
-            color = cm.nipy_spectral(float(i) / n_clusters)
-            ax1.fill_betweenx(np.arange(y_lower, y_upper),
-                              0, ith_cluster_silhouette_values,
-                              facecolor=color, edgecolor=color, alpha=0.7)
-
-            # Label the silhouette plots with their cluster numbers at the middle
-            ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
-
-            # Compute the new y_lower for next plot
-            y_lower = y_upper + 10  # 10 for the 0 samples
-
-        # ax1.set_title("The silhouette plot for the various clusters.")
-        # ax1.set_xlabel("The silhouette coefficient values")
-        ax1.set_ylabel("Cluster label")
-
-        # The vertical line for average silhouette score of all the values
-        ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
-
-        ax1.set_yticks([])  # Clear the yaxis labels / ticks
-        ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
-
-        plt.suptitle(("#clusters = %d" % n_clusters),
-                     fontsize=13)
-        plt.savefig("{0}/kscc_sm_sake{1}.pdf".format(output_folder, n_clusters), dpi=300)
-    # plt.show()
-    import csv
-    with open("{0}/{1}".format(output_folder,ofile), 'w') as fp:
-        out = csv.writer(fp)
-        out.writerows(map(lambda x: [x], result))
-    print("Finish!!!")
-
-if __name__ == "__main__":
-    run(sys.argv[1:])
+        assert hasattr(self, 'cluster_centroids_'), "Model not yet fitted"
+        return _labels_cost(X, self.cluster_centroids_)[0]
+   
